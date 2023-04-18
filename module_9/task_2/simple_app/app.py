fa7d8a44e1811766614896f4a0417f16c78c1d37
ildar <ildar62gtr@gmail.com>
2023-04-18 15:07:27 +0500

    модуль 9

---
+++ module_9/task_2/simple_app/app.py
@@ -0,0 +1,34 @@
+import datetime
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+HOST = "0.0.0.0"
+PORT = 5000
+
+@app.route("/good_time")
+def good_time():
+    return render_template('index.html')
+    # return f"Доброго времени суток, текущее время: {datetime.datetime.now().time()}"
+
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+
+@app.errorhandler(404)
+def list_routes(err):
+    result = ''
+    links = []
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    for link, name in links:
+        result += f'<a href="{link}">{link}<a></br>'
+    return "Страница не найдена, доступные страницы:</br>" + result
+
+
+if __name__ == '__main__':
+    app.run(host=HOST, port=PORT, debug=True)
